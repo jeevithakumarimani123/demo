e6e4ec909cc5c4d5672cdbdd5a5880fa49c47c4a
Jeevitha <jeevithakumari.mani123@gmail.com>
2026-05-06 15:09:05 +0530

    Docker changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
 	
 	@PostConstruct
 	public void init() {
-		logger.info("Demo Application started.slightly changes to check 99999 ioio");
+		logger.info("Demo Application started.slightly changes to check 99999 docker push");
 	}
 
 	public static void main(String[] args) {
